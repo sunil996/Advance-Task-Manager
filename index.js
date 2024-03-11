f67d91a501ebd0ef60a9462a5f4889a440857a38
sunil996 <sunilshekhawat772@gmail.com>
2024-03-11 17:51:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app =require("./app.js"); 
+const connectDB=require("./db/connection.js");
+
+connectDB()
+.then(()=>{
+    console.log('connected to the database.')
+    app.listen(process.env.Port,()=>console.log(`server is listening on ${process.env.PORT || 5000}`))
+})
+.catch((err)=>{
+    console.log("mongodb connection failed ",err);
+})

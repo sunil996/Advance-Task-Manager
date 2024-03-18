3008d726ad44ad199935aab1ed72595030ce978f
sunil996 <sunilshekhawat772@gmail.com>
2024-03-18 23:04:38 +0530

    chore: added routes in app.js file

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express=require("express");
 const errorHandler=require("./middlewares/errorHandler.middleware.js") 
-
+const cron = require('node-cron');
 const app=express();
  
-//middelware
+ 
 app.use(express.json());
 
-//routes
-
+//routes 
 const userRouter=require("./routes/user.routes.js")
+const collectionRouter=require("./routes/collection.routes.js");
+const taskRouter=require("./routes/task.routes.js");
+
 app.use("/api/v1/user", userRouter) 
+app.use("/api/v1/collection",collectionRouter);
+app.use("/api/v1/task",taskRouter);
+ 
+cron.schedule('0 0 * * 1-5', async () => {
+    
+    try {  
+    //  await removeUnverifiedUsers();
+    } catch (error) {
+        console.error('Error in cleanup job:', error.message);
+    }
+  });
 
+ 
 app.use(errorHandler)
-module.exports=app;
-  
-
-
+module.exports=app;  
